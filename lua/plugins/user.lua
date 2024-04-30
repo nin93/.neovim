06f88cab1e2da3ba5fb96a1db237f1b1fd39ea51
Elia Franzella <eliafranzella@live.it>
2024-05-01 01:58:35 +0200

    fix: Use window picker floating letter

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -34,8 +34,15 @@ return {
     end,
   },
 
-  -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
+
+  {
+    "s1n7ax/nvim-window-picker",
+    opts = function(_, opts)
+      opts.hint = "floating-big-letter"
+      return opts
+    end,
+  },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
