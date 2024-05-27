6f655b10b78b8ad983600b0863041950938195fe
Elia Franzella <eliafranzella@live.it>
2024-05-27 04:19:01 +0200

    feat: Use `onedarkpro` colorscheme

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,20 @@
+-- asdasdasda
+return {
+  "olimorris/onedarkpro.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    colors = {
+      dark = {
+        bg = "#171717",
+      },
+    },
+    styles = {
+      comments = "italic",
+    },
+    options = {
+      cursorline = false,
+      terminal_colors = false,
+    },
+  },
+}
